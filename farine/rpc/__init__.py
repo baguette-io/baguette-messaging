2f64268b8b497829c11253b3a96ecfab45e4480c
David Macro <david.macro7@gmail.com>
2017-02-21 17:17:35 -0800

    fix naming

--- farine/rpc/__init__.py
+++ farine/rpc/__init__.py
@@ -12,21 +12,21 @@ from farine.rpc.server import Server
 LOGGER = logging.getLogger(__name__)
 
 def client(service, exchange=None, routing_key=None):
-    def wrapper(method):
+    def wrapper(_method):
         def subwrapper(self, *args, **kwargs):
             _exchange = exchange or service
             _routing_key = routing_key or service
             if not hasattr(self, 'rpc'):
                 rpc = Client(_exchange, _routing_key, service=service)
-            return method(self, rpc, *args, **kwargs)
+            return _method(self, rpc, *args, **kwargs)
         return subwrapper
     return wrapper
 
 def method(*args, **kwargs):
-    def wrapper(method):
-        farine.discovery.ENTRYPOINTS.append((Server, method, args, kwargs))
+    def wrapper(_method):
+        farine.discovery.ENTRYPOINTS.append((Server, _method, args, kwargs))
         def subwrapper(self, *args, **kwargs):
-            LOGGER.info('Received message : %s %s , call %s.', args, kwargs, method.__name__)
-            return method(self, *args, **kwargs)
+            LOGGER.info('Received message : %s %s , call %s.', args, kwargs, _method.__name__)
+            return _method(self, *args, **kwargs)
         return subwrapper
     return wrapper
