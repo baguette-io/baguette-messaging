8269dd3748160c03416e94ea33c64195a6f88a55
David Macro <david.macro7@gmail.com>
2017-10-10 13:50:14 -0700

    bump 0.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 
 setup(name='baguette-messaging',
-      version='0.12',
+      version='0.13',
       description='Baguette messaging framework',
       long_description=open('README.rst').read(),
       url='https://github.com/baguette-io/baguette-messaging/',
